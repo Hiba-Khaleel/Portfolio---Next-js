8d6364d6f84ce7c34c171706f6a83b18449581ad
Hiba khaleel <hiba.khlaleel@gmail.com>
2024-01-25 23:27:22 +0100

    adding some points

--- myPortfolio/next.config.js
+++ myPortfolio/next.config.js
@@ -1,4 +1,36 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/public/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=3600", // 1 hour cache
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+};
